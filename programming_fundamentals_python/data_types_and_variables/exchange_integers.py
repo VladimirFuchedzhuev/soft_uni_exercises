82bb52514ca84b95a41b134f5a959bd23a7930d6
Vladimir <fuchedzhhiev@yahoo.com>
2021-09-18 22:57:54 +0300

    Added exercises

---
+++ programming_fundamentals_python/data_types_and_variables/exchange_integers.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+
+print("Before:")
+print(f"a = {a}")
+print(f"b = {b}")
+
+
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After:")
+print(f"a = {a}")
+print(f"b = {b}")
+
