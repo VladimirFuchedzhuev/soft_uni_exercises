153c511d80f153b78eb8f2d85f0d10a65d6794c8
Vladimir <fuchedzhhiev@yahoo.com>
2021-09-30 18:20:46 +0300

    Added exercises

---
+++ programming_fundamentals_python/dictionaries/orders.py
@@ -0,0 +1,19 @@
+data = input()
+products = {}
+
+
+while not data == "buy":
+    name, price, quantity = data.split()
+    price = float(price)
+    quantity = int(quantity)
+    if name not in products:
+        products[name] = {"price": price, "quantity": quantity}
+    else:
+        products[name]["quantity"] += quantity
+        if not products[name]["price"] == price:
+            products[name]["price"] = price
+    data = input()
+    
+for key, value in products.items():
+    result = value['price'] * value['quantity']
+    print(f"{key} -> {result:.2f}")
