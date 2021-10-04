72d029084d7ef85054eb7ce1f0e2149a6611d1b6
Vladimir <fuchedzhhiev@yahoo.com>
2021-10-04 18:05:53 +0300

    Added_exercises

---
+++ programming_fundamentals_python/dictionaries/statistics.py
@@ -0,0 +1,16 @@
+stock = input()
+products = {}
+
+while not stock == 'statistics':
+    key, value = stock.split(': ')
+    if key not in products:
+        products[key] = int(value)
+    else:
+        products[key] += int(value)
+    stock = input()
+
+print("Products in stock:")
+for key, value in products.items():
+    print(f"- {key}: {value}")
+print(f"Total Products: {len(products)}")
+print(f"Total Quantity: {sum(products.values())}")
