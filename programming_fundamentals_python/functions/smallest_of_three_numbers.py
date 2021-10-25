db77c90a4f5c61fe9caec74b41c10522d0ccba77
Vladimir <fuchedzhhiev@yahoo.com>
2021-10-25 21:04:26 +0300

    Added exercises

---
+++ programming_fundamentals_python/functions/smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+def smallet_of_three_numbers(num1, num2, num3):
+    return min(num1, num2, num3)
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(smallet_of_three_numbers(num1, num2, num3))
+
